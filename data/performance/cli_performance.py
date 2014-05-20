d7edecdead34e160a3426d4caaec219c5f1b69eb
Fabian Schindler <fabian.schindler@gmx.at>
2014-05-20 17:37:15 +0200

    Improving CLI script.

--- data/performance/cli_performance.py
+++ data/performance/cli_performance.py
@@ -27,9 +27,9 @@
 
 import csv
 import argparse
-import time
 import os
-from time import gmtime, strftime
+from os.path import join
+from datetime import datetime
 
 
 #class to allow easier timing of method calls
@@ -43,126 +43,135 @@ class Timer:
         self.interval = self.end - self.start
 
 
+def iterate_browses(csv_path, basepath):
+    if basepath:
+        csv_path = join(basepath, csv_path)
 
-#argument specification for script
-parser = argparse.ArgumentParser(description='Command line performance test tool')
+    ns = {"rep": "http://ngeo.eo.esa.int/schema/browseReport"}
+    with open(csv_path) as f:
+        reader = csv.reader(f)
+        for row in reader:
 
-parser.add_argument('--ingest', nargs=3,
-                   help='Ingestion of browse reports listed in csv file. Pass path to csv file ,path to browse reports, path to storage and and output csv file as arguments.')
+            report_filename = row[0]
+            if basepath:
+                report_filename = join(basepath, report_filename)
 
-parser.add_argument('--export', nargs=2,
-                   help='Specify export csv with chunks definition file and export path')
+            tree = etree.parse(report_filename)
+            browse_type = tree.xpath("/rep:browseType/text()", namespaces=ns)[0]
+            starts = tree.xpath("/rep:browse/rep:startTime/text()", namespaces=ns)
+            ends = tree.xpath("/rep:browse/rep:endTime/text()", namespaces=ns)
+            for start, end in izip(starts, ends):
+                yield report_filename, browse_type, start, end
 
-parser.add_argument('--delete', nargs=1,
-                   help='Specify csv file with chunks to be deleted')
 
-parser.add_argument('--import', nargs=1, dest='import_dest',
-                   help='Specify path to import csv script')
+def execute(command, out_writer=None):
+    """ Executes a command line command and returns the time in seconds.
+    """
+    with Timer() as timer:
+        code = os.system(command)
 
-parser.add_argument('--test', nargs=5,
-                   help='Do complete performance test, composed of ingest, export, delete and import. Specify path to csv file, path to folder with images and output csv file')
+    if out_writer:
+        out_writer.write_row(command, "%.09f" % timer.interval, str(code))
 
-parser.add_argument('--output', nargs=1,
-                   help='Ingestion of browse reports listed in csv file. Pass path to csv file ,path to browse reports, path to storage and and output csv file as arguments.')
-
-parser.add_argument('--debug', action='store_true', help='print debug messages to stderr')
+    return timer.interval, code
 
 
 #definition for performance test methods
-def ingest(csvfilePath, browsePath, storagePath, output, debug):
-    f = open(output+'ingest_report'+strftime("%Y-%m-%d_%H:%M:%S", gmtime())+'.csv','w')
-    f.write('cmd,execution_time,return_code\n')     
-    with open(csvfilePath, 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        os.system("echo ======================================================================")
-        os.system("echo ============= Performance test starting for ingest====================")
-        os.system("echo ======================================================================")
-        for row in reader:
-            with Timer() as t:
-                #execute ingest command
-                if(not debug):
-                    cmd = "python ../manage.py ngeo_ingest --storage-dir " + storagePath + " --leave-original " + browsePath + row[0]
-                else:
-                    cmd = "python ../manage.py ngeo_ingest --storage-dir " + storagePath + " --leave-original " + browsePath + row[0] + " -v3" + " --traceback"
-                code = os.system(cmd)
-            f.write(cmd +","+ '%.09f,%s'%(t.interval,code)+'\n') 
+def ingest(iterator, out_writer, browse_dir):
+    for report_filename, _, _, _ in iterator:
+        command = "python ../../manage.py ngeo_ingest %s" % report_filename
+        if browse_dir:
+            command = "%s --leave-original --storage-dir %s" % (command, browse_dir)
+        execute(command, out_writer)
+
+
+def export(iterator, out_writer, export_path):
+    for _, browse_type, start, end in iterator:
+        out_filename = join(export_path, "%s_%s_%s.tar.gz" % (browse_type, start, end))
+        command = (
+            "python ../../manage.py ngeo_export --browse-type %s --start %s --end %s --output %s " 
+            % (browse_type, start, end, out_filename)
+        )
+        execute(command, out_writer)
+
+
+def delete(iterator, out_writer):
+    for _, browse_type, start, end in iterator:
+        command = (
+            "python ../../manage.py ngeo_delete --browse_type %s --start %s --end %s " 
+            % (browse_type, start, end)
+        )
+        execute(command, out_writer)
+
+
+def import_(import_dir, out_writer):
+    for path in glob(join(import_dir, "*.tar.gz")):
+        command = "python ../../manage.py ngeo_import %s" % path
+        execute(command, out_writer)
             
 
-
-def export(csvfilePath, exportPath, output , debug):
-    f = open(output+'export_report'+strftime("%Y-%m-%d_%H:%M:%S", gmtime())+'.csv','w')
-    f.write('cmd,execution_time,return_code\n')     
-    with open(csvfilePath, 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        os.system("echo ======================================================================")
-        os.system("echo ============= Performance test starting for export====================")
-        os.system("echo ======================================================================")
-        for row in reader:
-            with Timer() as t:
-                if(not debug):
-                    cmd = "python ../manage.py ngeo_export --layer %s --start %s --end %s --output %s "%(row[0],row[1],row[2],(exportPath+row[0]+row[1]+row[2]+".tar.gz"))
-                else:
-                    cmd = "python ../manage.py ngeo_export --layer %s --start %s --end %s --output %s -v3 --traceback"%(row[0],row[1],row[2],(exportPath+row[0]+row[1]+row[2]+".tar.gz"))
-                code = os.system(cmd)
-            f.write(cmd +","+ '%.09f,%s'%(t.interval,code)+'\n') 
+def get_writer(command, time):
+    f = open("%s_%s.csv" % (command, time), "w")
+    return csv.writer(f)
 
 
+if __name__ == "__main__":
+    #argument specification for script
+    parser = argparse.ArgumentParser(description='Command line performance test tool')
 
-def delete(csvfilePath, output , debug):
-    f = open(output+'delete_report'+strftime("%Y-%m-%d_%H:%M:%S", gmtime())+'.csv','w')
-    f.write('cmd,execution_time,return_code\n')     
-    with open(csvfilePath, 'rb') as csvfile:
-        reader = csv.reader(csvfile)
-        os.system("echo ======================================================================")
-        os.system("echo ============= Performance test starting for delete====================")
-        os.system("echo ======================================================================")
-        for row in reader:
-            with Timer() as t:
-                if(not debug):
-                    cmd = "python ../manage.py ngeo_delete --layer %s --start %s --end %s "%(row[0],row[1],row[2])
-                else:
-                    cmd = "python ../manage.py ngeo_delete --layer %s --start %s --end %s -v3 --traceback"%(row[0],row[1],row[2])
-                code = os.system(cmd)
-            f.write(cmd +","+ '%.09f,%s'%(t.interval,code)+'\n') 
+    parser.add_argument(
+        '--ingest', action="store_true",
+        help='Ingestion of browse reports listed in csv file.'
+    )
+    parser.add_argument(
+        '--export', action="store_true",
+        help='Export all browses referenced by any browse report in the csv file.'
+    )
+    parser.add_argument(
+        '--delete', action="store_true",
+        help='Delete all browses reference by any browse report in the csv file.'
+    )
+    parser.add_argument(
+        '--import', dest="import_", action="store_true",
+        help='Import all archives in the "export-dir".'
+    )
+    parser.add_argument(
+        '--all', action="store_true",
+        help='Do complete performance test, composed of ingest, export, delete and import.'
+    )
+    parser.add_argument(
+        '--export-dir', dest="export_dir",
+        help="Optional. Specify location to store exported archives. Used for export and import."
+    )
+    parser.add_argument(
+        '--report-dir', default=None,
+        help="Optional. Specify the location of the browse reports. Used for ingest, export and delete."
+    )
+    parser.add_argument(
+        '--browse-dir', default=None,
+        help="Optional. Specify the location of the browse. Used for ingest."
+    )
+    parser.add_argument(
+        'input_csv', nargs=1, dest="browse_report_csv_path"
+        help='The input csv file that references the browse reports to be tested.'
+    )
 
+    args = parser.parse_args()
 
+    now = datetime.now().isoformat("T")
+    csv_path = args.browse_report_csv_path[0]
+    report_dir = args.report_dir
+    browse_dir = args.browse_dir
+    export_dir = args.export_dir
 
-def import_file(importfolder, output, debug):
-    f = open(output+'import_report'+strftime("%Y-%m-%d_%H:%M:%S", gmtime())+'.csv','w')
-    f.write('cmd,execution_time,return_code\n') 
-    os.system("echo ======================================================================")
-    os.system("echo ============= Performance test starting for import====================")
-    os.system("echo ======================================================================")
-    for files in os.listdir(importfolder):  
-        with Timer() as t:
-            if(not debug):
-                cmd = "python ../manage.py ngeo_import %s "%(importfolder+files)
-            else:
-                cmd = "python ../manage.py ngeo_import %s -v3 --traceback"%(importfolder+files)
-            print(cmd)
-            code = os.system(cmd)
-        f.write(cmd +","+ '%.09f,%s'%(t.interval,code)+'\n') 
-            
+    if args.ingest or args.all:
+        ingest(iterate_browses(csv_path, report_dir), get_writer("ingest", now), browse_dir)
 
+    if args.export or args.all:
+        export(iterate_browses(csv_path, report_dir), get_writer("export", now), export_dir)
 
-args = parser.parse_args()
+    if args.delete or args.all:
+        delete(iterate_browses(csv_path, report_dir), get_writer("delete", now))
 
-if (args.output):
-
-    if (args.ingest):
-        ingest(args.ingest[0], args.ingest[1], args.ingest[2], args.output[0], args.debug)
-
-    if(args.export):
-        export(args.export[0], args.export[1], args.output[0], args.debug)
-
-    if (args.delete):
-        delete(args.delete[0], args.output[0],args.debug)
-
-    if(args.import_dest):
-        import_file(args.import_dest[0], args.output[0], args.debug)
-
-    if (args.test):
-        ingest(args.test[0], args.test[1], args.test[2], args.output[0], args.debug)
-        export(args.test[3], args.test[4], args.output[0], args.debug)
-        delete(args.test[3], args.output[0], args.debug)
-        import_file(args.test[4], args.output[0], args.debug)
+    if args.import_ or args.all:
+        import_(export_dir, get_writer("import", now))
